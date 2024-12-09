c48d3ac157e706bc75580a9d9a107d813374a9fa
Igor <igor.lakic2@gmail.com>
2024-12-09 20:57:22 +0100

    file structure, added basic files

---
+++ src/Home.tsx
@@ -0,0 +1,15 @@
+import AppTopbar from "./AppTopbar";
+import Content from "./Content";
+
+const Home = () => {
+  return (
+    <div>
+      <main>
+        <AppTopbar />
+        <Content />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
